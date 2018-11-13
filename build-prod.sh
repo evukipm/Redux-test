eed55a31c33b0e08a98996ed9d49bc7b7fa5dc4a
Óscar Medina <oscmedgon@gmail.com>
2018-11-13 19:07:00 +0100

    fixed launchers env

--- build-prod.sh
+++ build-prod.sh
@@ -2,4 +2,4 @@
 echo "Cleaning build directory"
 rm -rf build
 echo "Building new app"
-webpack --env.ENV=production --config webpack.config.prod.js -p
+npm run build
--- server.sh
+++ server.sh
@@ -8,4 +8,4 @@ nvm use 8
 echo "Cleaning dist directory"
 rm -rf dist
 echo "Building new app"
-./node_modules/.bin/webpack-dev-server --env.ENV=development --config webpack.config.js
+npm run dev
